88c8f6cde1e76c6dbc6e856997714c6626f6703a
Big.teeradet <scms.dev01@gmail.com>
2023-10-02 20:52:22 +0700

    init: spring boot

---
+++ backend/src/main/java/sit/CP23EJ2/bannarug/BannarugApplication.java
@@ -0,0 +1,13 @@
+package sit.CP23EJ2.bannarug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannarugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannarugApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sit/CP23EJ2/bannarug/BannarugApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.CP23EJ2.bannarug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannarugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
