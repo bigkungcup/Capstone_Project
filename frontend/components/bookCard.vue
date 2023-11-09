2375c4275c0780e93a43594455b5fca1791ebc09
Taradon63130500058 <taradon.job@mail.kmutt.ac.th>
2023-11-09 23:16:40 +0700

    add: book card

--- frontend/components/bookCard.vue
+++ frontend/components/bookCard.vue
@@ -8,18 +8,18 @@ defineProps({
 </script>
  
 <template>
-    <div class="tw-px-24">
-        <v-card v-for="book in bookList" height="30%" color="#D9D9D9">          
+    <div class="tw-h-screen tw-px-28">
+        <v-card v-for="book in bookList" height="40%" color="#D9D9D9">          
             <v-row no-gutters>
-                <v-col class="tw-grid tw-place-items-center">
-                <img src="/image/cover_not_available.jpg" width="50%" spect-ratio="16/9" cover />
+                <v-col class="tw-my-4" align="center">
+                <v-img src="/image/cover_not_available.jpg" width="40%"/>
                 </v-col>
                 <v-col class="tw-place-content-center">
                     <div>
                         <p>{{book.bookName}}</p>
                         <p>Update about {{ }}</p>
                     </div>
-                    <div class="tw-h-3/5 tw-py-2 tw-overflow-hidden">
+                    <div class="tw-h-2/5 tw-py-2 tw-overflow-hidden">
                         <p class="tw-text-ellipsis">{{book.bookDetail}}</p>
                     </div>
                     <div>{{book.bookTotalReview}} reviews</div>
