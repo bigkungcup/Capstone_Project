96ddd0a28dee86c4a0ced3596ffeadaa948ab823
Taradon63130500058 <taradon.job@mail.kmutt.ac.th>
2023-11-14 00:13:39 +0700

    add: library pagination

--- frontend/components/bookCard.vue
+++ frontend/components/bookCard.vue
@@ -1,44 +1,52 @@
 <script setup>
 defineProps({
-  bookList: {
-    type: Array,
-    require: true,
-  }
+    bookList: {
+        type: Array,
+        require: true,
+    }
 })
 </script>
  
 <template>
-    <div class="tw-h-screen tw-px-28">
-        <v-card v-for="book in bookList" height="40%" color="#D9D9D9">          
+    <div class="h-100 tw-px-24">
+        <v-card v-for="book in bookList" color="#D9D9D9" class="tw-mb-4 tw-h-2/5" :to="`/book/${book.bookId}`">
             <v-row no-gutters>
-                <v-col class="tw-my-4" align="center">
-                <v-img src="/image/cover_not_available.jpg" width="40%"/>
+                <v-col cols="2" class="tw-my-4" align="center">
+                    <v-img src="/image/cover_not_available.jpg" width="80%" />
                 </v-col>
-                <v-col class="tw-place-content-center">
+                <v-col cols="7" class="web-text-detail tw-my-8 tw-mx-2 tw-space-y-0.5">
                     <div>
-                        <p>{{book.bookName}}</p>
-                        <p>Update about {{ }}</p>
+                        <p class="web-text-title">{{ book.bookName }}</p>
+                        <p class="tw-opacity-60">Update about {{ }}</p>
                     </div>
-                    <div class="tw-h-2/5 tw-py-2 tw-overflow-hidden">
-                        <p class="tw-text-ellipsis">{{book.bookDetail}}</p>
+                    <div class="tw-h-2/6 tw-py-2 tw-overflow-hidden">
+                        <p class="tw-text-ellipsis tw-indent-8">{{ book.bookDetail }}</p>
                     </div>
-                    <div>{{book.bookTotalReview}} reviews</div>
+                    <div class="">{{ book.bookTotalReview }} reviews</div>
                 </v-col>
-                <v-col>
-                    <div class="tw-flex">
-                        <p>Book Type : </p>
-                        <span>{{book.bookType}}</span>
+                <v-col class="tw-grid tw-grid-cols-8 tw-gap-2">
+                    <div class="vertical-line tw-my-8"></div>
+                    <div class="web-text-detail tw-col-span-7 tw-py-12">
+                        <div class="tw-flex tw-gap-x-2 tw-py-2 tw-items-center">
+                            <p class="tw-font-bold">Book Type : </p>
+                            <span class="tag-color">{{ book.bookType }}</span>
+                        </div>
+                        <div class="tw-flex tw-gap-x-4 tw-py-2 tw-items-center">
+                            <p class="tw-font-bold">Genre : </p>
+                            <span class="tag-color">{{ book.bookGenre }}</span>
                     </div>
-                    <div class="tw-flex">
-                        <p>Genre : </p>
-                        <span>{{book.bookGenre}}</span>
-                    </div>
+                </div>
                 </v-col>
             </v-row>
         </v-card>
     </div>
 </template>
  
-<style>
-
+<style scoped>
+.vertical-line{
+    border-left: 4px solid #082266;
+    opacity: 50%;
+    height: 50%;
+    justify-self: center;
+}
 </style>
