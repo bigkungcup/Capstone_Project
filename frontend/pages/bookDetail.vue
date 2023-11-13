f02bc731e907f1cdd44a3b042c7ba927b11aae01
malamild <anonymike09@gmail.com>
2023-11-13 21:45:51 +0700

    add: data in bookDetail page

--- frontend/pages/bookDetail.vue
+++ frontend/pages/bookDetail.vue
@@ -1,33 +1,54 @@
 <script setup>
 
+const library = {
+  data: [    
+    {
+      bookId: '01',
+      bookName: 'I Know Why the Caged Bird Sings',
+      bookDetail: 'Maya Angelou’s debut memoir is a modern American classic beloved worldwide. Her life story is told in the documentary film And Still I Rise, as seen on PBS’s American Masters.Here is a book as joyous and painful, as mysterious and memorable, as childhood itself. I Know Why the Caged Bird Sings captures the longing of lonely children, the brute insult of bigotry, and the wonder of words that can make the world right. Maya Angelou’s debut memoir is a modern American classic beloved worldwide. Sent by their mother to live with their devout, self-sufficient grandmother in a small Southern town, Maya and her brother, Bailey, endure the ache of abandonment and the prejudice of the local “powhitetrash.” At eight years old and back at her mother’s side in St. Louis, Maya is attacked by a man many times her age—and has to live with the consequences for a lifetime.',
+      author: 'Maya Angelou',
+      bookType: 'Autobiography',
+      bookGenre: 'Biography',
+      bookTotalBookmarked: 124,
+      bookTotalReview: 3,
+      bookRating: 5.0,
+      updateDate: '',
+      bookCover: null
+    }
+  ]
+}
 </script>
  
 <template>
     <div class="ma-2"><v-icon end icon="mdi mdi-chevron-left"></v-icon> Back </div>
     <div class="tw-flex tw-justify-center tw-h-3/5 tw-max-h-[30rem] tw-min-h-[30rem]">
-        <v-card color="#D9D9D9" theme="dark" width="80%">
-            <v-row>
+        <v-card class="bg-opct" color="#D9D9D9"  width="80%">
+            <!-- <v-row no-gutters>
+                <v-col cols="10"></v-col>
+                <v-col cols="2" class="tw-flex tw-justify-center"><v-btn >Report</v-btn></v-col>
+            </v-row> -->
+            <v-row no-gutters>
                 <v-col cols="4" class="tw-my-4" align="center">
                     <v-img src="/image/bookcover.png" height="60%" width="100%"></v-img>
                     <v-img src="/image/star.png" width="50%"></v-img>
                 </v-col>
-                <v-col cols="8" class="tw-my-4 tw-space-y-10">
-                    <v-card-title class="text-h4 tw-text-[#082266]">
-                        Book's Name
-                    </v-card-title>
+                <v-col cols="6" class="tw-my-4 tw-space-y-10">
+                    <p class="text-h4 tw-text-[#082266]">
+                        {{library.data[0].bookName}}
+                    </p>
 
-                    <v-card-subtitle class="text-h6 tw-text-[#1D419F]">
-                        Author: Edogawa Conan
-                    </v-card-subtitle>
-                    <v-card-subtitle class="text-h6 tw-text-[#1D419F]">
-                        Bookmarked by: 200 people
-                    </v-card-subtitle>
-                    <v-card-subtitle class="text-h6 tw-text-[#1D419F]">
-                        Booktype:
-                    </v-card-subtitle>
-                    <v-card-subtitle class="text-h6 tw-text-[#1D419F]">
-                        Genre:
-                    </v-card-subtitle>
+                    <p class="text-h5 tw-text-[#1D419F]">
+                        Author: {{library.data[0].author}}
+                    </p>
+                    <p class="text-h6 tw-text-[#1D419F]">
+                        Bookmarked by: {{library.data[0].bookTotalBookmarked}} people
+                    </p>
+                    <p class="text-h6 tw-text-[#1D419F]">
+                        Booktype: <v-btn color="#1D419F">{{library.data[0].bookType}}</v-btn>
+                    </p>
+                    <p class="text-h6 tw-text-[#1D419F]">
+                        Genre: <v-btn color="#1D419F">{{library.data[0].bookGenre}}</v-btn>
+                    </p>
                     <div class="tw-flex tw-justify-center tw-gap-x-12">
                         <v-btn class="text-none" color="#1D419F"><v-icon start
                                 icon="mdi mdi-bookmark"></v-icon>Bookmark</v-btn>
@@ -35,6 +56,7 @@
                                 icon="mdi mdi-pencil-plus"></v-icon>Review</v-btn>
                     </div>
                 </v-col>
+                <v-col cols="2" class="tw-flex tw-justify-center my-2"><v-btn >Report</v-btn></v-col>
             </v-row>
 
         </v-card>
@@ -42,9 +64,9 @@
     </div>
 
     <div class="tw-flex tw-justify-center my-4">
-        <v-card color="#D9D9D9" width="80%">
+        <v-card class="bg-opct" color="#D9D9D9" width="80%">
             <div class="tw-bg-white tw-flex tw-justify-center tw-m-5 tw-p-4 tw-text-xl tw-rounded-md">
-                Maya Angelou’s debut memoir is a modern American classic beloved worldwide.
+                <!-- Maya Angelou’s debut memoir is a modern American classic beloved worldwide.
                 Her life story is told in the documentary film And Still I Rise, as seen on PBS’s American Masters.Here is a
                 book as joyous and painful,
                 as mysterious and memorable, as childhood itself. I Know Why the Caged Bird Sings captures the longing of
@@ -54,18 +76,18 @@
                 self-sufficient grandmother in a small Southern town, Maya and her brother, Bailey, endure the ache of
                 abandonment
                 and the prejudice of the local “powhitetrash.” At eight years old and back at her mother’s side in St.
-                Louis, Maya is attacked by a man many times her age—and has to live with the consequences for a lifetime.
-
+                Louis, Maya is attacked by a man many times her age—and has to live with the consequences for a lifetime. -->
+                {{library.data[0].bookDetail}}
             </div>
         </v-card>
     </div>
 
-    <div class="tw-flex tw-justify-left my-2 tw-bg-[#D9D9D9] tw-min-h-[24rem]">
+    <div class="tw-flex tw-justify-left my-2 tw-bg-[#D9D9D9] tw-min-h-[24rem] bg-opct">
         <p class="tw-text-4xl tw-text-[#082266] tw-px-6 tw-py-2">Similar Book</p>
     </div>
 
     <div class="tw-flex tw-justify-center my-2 ">
-        <div class="tw-bg-[#D9D9D9] tw-w-10/12 tw-min-h-[60rem] tw-rounded-lg">
+        <div class="tw-bg-[#D9D9D9] tw-w-10/12 tw-min-h-[60rem] tw-rounded-lg bg-opct">
             <p class="tw-text-3xl tw-text-[#082266] tw-px-6 tw-py-2"> Review (0): </p>
             <div class="tw-bg-white tw-flex tw-justify-center tw-m-5 tw-p-4 tw-min-h-[56rem] tw-rounded-lg">
                 <v-container>
@@ -80,7 +102,7 @@
                                 Filter
                             </v-btn></v-col>
                     </v-row>
-                    <v-row> <v-img src="/image/rvnotfound.png" width="50%"></v-img></v-row>
+                    <v-row> <v-img src="/image/rvnotfound.png" width="40%" class="tw-opacity-50"></v-img></v-row>
                 </v-container>
                
             </div>
@@ -88,4 +110,13 @@
     </div>
 </template>
  
-<style></style>
+<style scoped>
+
+.bg-opct {
+    background-color: rgb(217, 217, 217, 0.6) !important;
+    color: rgb(0, 0, 0) !important;
+    caret-color: rgb(0, 0, 0) !important;
+    /* width: 80% !important; */
+}
+
+</style>
