5ad33816f4aeb4e5bf5f47c617936c1ad4387103
Big.trd <bigkungcup2@hotmail.co.th>
2023-12-11 00:36:21 +0700

    update: backend validation, docker compose, nginx.conf

--- backend/src/main/java/sit/cp23ej2/dtos/Review/UpdateReviewDTO.java
+++ backend/src/main/java/sit/cp23ej2/dtos/Review/UpdateReviewDTO.java
@@ -1,5 +1,6 @@
 package sit.cp23ej2.dtos.Review;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -18,6 +19,7 @@ public class UpdateReviewDTO {
     private String detail;
 
     @NotBlank(message = "Title shouldn't be blank or null")
+    @Max(value = 255, message = "Title shouldn't be more than 255 characters")
     private String title;
 
     @NotNull(message = "SpoileFlag shouldn't be null")
