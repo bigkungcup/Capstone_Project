da19acef36c10a01e17e0b6aeb02c56d6fd40562
malamild <anonymike09@gmail.com>
2024-02-13 22:53:07 +0700

    add: history page

---
+++ frontend/components/history/historyCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+ 
+<template>
+    <!-- <div  class="tw-px-36 tw-space-y-4"> -->
+    <v-card color="rgb(217, 217, 217, 0.6)" class="tw-h-[12rem] ">
+        <v-row no-gutters>
+            <v-col cols="4" class="tw-my-4" align="center">
+                <v-img class="tw-drop-shadow-lg " src="/image/foryou1.png" width="100" height="160" cover />
+            </v-col>
+            <v-col cols="8" class="tw-my-4">
+                <div class="">
+                        <p  class="web-text-title tw-truncate" >ALONE</p>
+                    <div class="tw-space-x-2">
+                        <v-icon icon="mdi mdi-account" color="#082266"></v-icon>
+                        <span class="web-text-sub">Maya Angelou</span>
+                    </div>
+                    <v-row no-gutters>
+                        <v-col cols="10">
+                            <p class=" tw-my-16 web-text-sub"> About a few second ago </p>
+                        </v-col>
+                        <v-col cols="2">
+                            <v-icon icon="mdi mdi-trash-can-outline" color="#8B8B8B" class="tw-my-16" style="font-size: 2rem;"></v-icon>
+                        </v-col>
+                    </v-row>
+                </div>
+            </v-col>
+        </v-row>
+    </v-card>
+    <!-- </div> -->
+</template>
+ 
+<style></style>
---
+++ frontend/pages/history/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import HistoryCard from '~/components/history/historyCard.vue';
+
+</script>
+ 
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="12" align="right">
+                    <v-btn color="#1D419F" rounded variant="flat" size="large"
+                        prepend-icon="mdi mdi-trash-can-outline">Clear all</v-btn>
+                </v-col>
+            </v-row>
+            <v-row >
+                <v-col cols="6" class="">
+                    <HistoryCard />
+                </v-col>
+                <v-col cols="6">
+                    <HistoryCard />
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+ 
+<style></style>
