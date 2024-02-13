37ca4cac30a75bc9633ee39bb9b7345c89014e27
malamild <anonymike09@gmail.com>
2024-02-13 23:12:00 +0700

    test: test on server

---
+++ frontend/pages/test/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useBooks } from '~/stores/book'
+
+const library = useBooks();
+const page = ref(1)
+
+onBeforeMount( async () => {
+  await library.getLibrary();
+});
+
+</script>
+ 
+<template>
+<div>
+    {{ library.bookList.data.content }}
+</div>
+</template>
+ 
+<style>
+
+</style>
