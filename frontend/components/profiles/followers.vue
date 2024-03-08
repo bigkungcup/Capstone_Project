a6050e7c36a4ad12b078f9c9d05a75f734aa9d3a
malamild <anonymike09@gmail.com>
2024-03-09 00:03:57 +0700

    add: following&follower in profile page

---
+++ frontend/components/profiles/followers.vue
@@ -0,0 +1,90 @@
+<script setup></script>
+
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="4">
+        <v-card class="tw-w-[20rem] tw-h-[20rem]">
+          <div class="tw-h-[8rem]">
+            <v-img src="/image/profile_banner.jpg" cover></v-img>
+          </div>
+
+          <div class="tw-flex tw-place-content-center">
+            <div class="tw-w-full tw-h-full">
+              <v-img
+                src="/image/guest_icon.png"
+                width="120"
+                height="120"
+                class="tw-rounded-full tw-border-white tw-border-8 tw-my-[-4rem] tw-mx-6"
+                cover
+              />
+            </div>
+            <v-btn color="#1D419F" variant="outlined" class="tw-m-2"
+              >Follow</v-btn
+            >
+          </div>
+          <div class="tw-my-[1rem] tw-mx-4">
+            <p class="web-text-title">Cat the reviewer</p>
+            <p class="web-text-pf-email">cat@gmail.com</p>
+            <p class="web-text-detail tw-my-2">I am God</p>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card class="tw-w-[20rem] tw-h-[20rem]">
+          <div class="tw-h-[8rem]">
+            <v-img src="/image/profile_banner.jpg" cover></v-img>
+          </div>
+
+          <div class="tw-flex tw-place-content-center">
+            <div class="tw-w-full tw-h-full">
+              <v-img
+                src="/image/guest_icon.png"
+                width="120"
+                height="120"
+                class="tw-rounded-full tw-border-white tw-border-8 tw-my-[-4rem] tw-mx-6"
+                cover
+              />
+            </div>
+            <v-btn color="#1D419F" variant="outlined" class="tw-m-2"
+              >Follow</v-btn
+            >
+          </div>
+          <div class="tw-my-[1rem] tw-mx-4">
+            <p class="web-text-title">Cat the reviewer</p>
+            <p class="web-text-pf-email">cat@gmail.com</p>
+            <p class="web-text-detail tw-my-2">I am God</p>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card class="tw-w-[20rem] tw-h-[20rem]">
+          <div class="tw-h-[8rem]">
+            <v-img src="/image/profile_banner.jpg" cover></v-img>
+          </div>
+
+          <div class="tw-flex tw-place-content-center">
+            <div class="tw-w-full tw-h-full">
+              <v-img
+                src="/image/guest_icon.png"
+                width="120"
+                height="120"
+                class="tw-rounded-full tw-border-white tw-border-8 tw-my-[-4rem] tw-mx-6"
+                cover
+              />
+            </div>
+            <v-btn color="#1D419F" variant="outlined" class="tw-m-2"
+              >Follow</v-btn>
+          </div>
+          <div class="tw-my-[1rem] tw-mx-4">
+            <p class="web-text-title">Cat the reviewer</p>
+            <p class="web-text-pf-email">cat@gmail.com</p>
+            <p class="web-text-detail tw-my-2">I am God</p>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped></style>
