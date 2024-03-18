5b3a2240fe8051289700ae59a81294bcbf291eaa
malamild <anonymike09@gmail.com>
2024-03-18 23:41:20 +0700

    add: user ranking

---
+++ frontend/components/ranking/userRank.vue
@@ -0,0 +1,169 @@
+<script setup></script>
+
+<template>
+  <div>
+
+    <div class="tw-my-8">
+      <v-row no-gutters>
+        <v-col cols="4" align="right">
+          <div class="tw-pt-24">
+            <v-img
+              src="/image/cat.jpg"
+              width="180"
+              height="180"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </div>
+        </v-col>
+        <v-col cols="4" align="center">
+          <div class="">
+            <v-img
+              src="/image/cat.jpg"
+              width="250"
+              height="250"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </div>
+        </v-col>
+        <v-col cols="4" align="left">
+          <div class="tw-pt-24">
+            <v-img
+              src="/image/cat.jpg"
+              width="180"
+              height="180"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </div>
+        </v-col>
+      </v-row>
+      <div>
+        <v-row no-gutters>
+          <v-col cols="1"></v-col>
+          <v-col cols="10" class="d-inline-flex">
+            <v-img src="/image/rankbanner2.png"></v-img>
+          </v-col>
+          <v-col cols="1"></v-col>
+        </v-row>
+      </div>
+    </div>
+
+    <div class="tw-my-24">
+      <div class="tw-h-min-[11rem] tw-h-max-[11rem]">
+        <hr class="tw-my-4" />
+        <v-row no-gutters>
+          <v-col cols="1" align="left" class="tw-my-8">
+            <p class="ranking-num-header">4</p>
+          </v-col>
+          <v-col
+            cols="8"
+            class="tw-flex tw-flex-col tw-justify-center tw-space-y-2"
+          >
+            <div class="tw-space-x-3 tw-inline-flex tw-items-center">
+              <span class="web-text-header">My Mate Nate</span>
+              <v-btn color="#1D419F" variant="outlined" rounded="xl"
+                >Follow</v-btn
+              >
+            </div>
+            <p class="web-text-sub web-text-pf-email">Cat@gmail.com</p>
+            <div
+              class="tw-space-x-1 tw-inline-flex tw-items-center tw-w-4/6 tw-py-1"
+            >
+              <span class="web-text-sub">12.2k followers</span>
+              <span class="web-text-sub">121 followings</span>
+            </div>
+          </v-col>
+          <v-col cols="1"></v-col>
+          <v-col cols="2" align="right">
+            <v-img
+              src="/image/cat.jpg"
+              width="150"
+              height="150"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </v-col>
+        </v-row>
+      </div>
+
+      <div class="tw-h-min-[11rem] tw-h-max-[11rem]">
+        <hr class="tw-my-4" />
+        <v-row no-gutters>
+          <v-col cols="1" align="left" class="tw-my-8">
+            <p class="ranking-num-header">5</p>
+          </v-col>
+          <v-col
+            cols="8"
+            class="tw-flex tw-flex-col tw-justify-center tw-space-y-2"
+          >
+            <div class="tw-space-x-3 tw-inline-flex tw-items-center">
+              <span class="web-text-header">My Mate Nate</span>
+              <v-btn color="#1D419F" variant="outlined" rounded="xl"
+                >Follow</v-btn
+              >
+            </div>
+            <p class="web-text-sub web-text-pf-email">Cat@gmail.com</p>
+            <div
+              class="tw-space-x-1 tw-inline-flex tw-items-center tw-w-4/6 tw-py-1"
+            >
+              <span class="web-text-sub">12.2k followers</span>
+              <span class="web-text-sub">121 followings</span>
+            </div>
+          </v-col>
+          <v-col cols="1"></v-col>
+          <v-col cols="2" align="right">
+            <v-img
+              src="/image/cat.jpg"
+              width="150"
+              height="150"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </v-col>
+        </v-row>
+      </div>
+
+      <div class="tw-h-min-[11rem] tw-h-max-[11rem]">
+        <hr class="tw-my-4" />
+        <v-row no-gutters>
+          <v-col cols="1" align="left" class="tw-my-8">
+            <p class="ranking-num-header">6</p>
+          </v-col>
+          <v-col
+            cols="8"
+            class="tw-flex tw-flex-col tw-justify-center tw-space-y-2"
+          >
+            <div class="tw-space-x-3 tw-inline-flex tw-items-center">
+              <span class="web-text-header">My Mate Nate</span>
+              <v-btn color="#1D419F" variant="outlined" rounded="xl"
+                >Follow</v-btn
+              >
+            </div>
+            <p class="web-text-sub web-text-pf-email">Cat@gmail.com</p>
+            <div
+              class="tw-space-x-1 tw-inline-flex tw-items-center tw-w-4/6 tw-py-1"
+            >
+              <span class="web-text-sub">12.2k followers</span>
+              <span class="web-text-sub">121 followings</span>
+            </div>
+          </v-col>
+          <v-col cols="1"></v-col>
+          <v-col cols="2" align="right">
+            <v-img
+              src="/image/cat.jpg"
+              width="150"
+              height="150"
+              class="tw-rounded-full tw-border-white tw-border-8"
+              cover
+            />
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<style></style>
